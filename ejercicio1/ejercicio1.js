fcd178ed6cdb0fbc192ff2902d241e01e3bf871a
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-07-23 02:25:07 +0100

    1

---
+++ ejercicio1/ejercicio1.js
@@ -0,0 +1,31 @@
+//1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+
+const showMeButton = document.querySelector(".showme");
+console.log(showMeButton);
+
+//1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+
+const h1Element = document.querySelector("#pillado");
+console.log(h1Element);
+
+//1.3 Usa querySelector para mostrar por consola todos los p
+
+const allParagraphs = document.querySelectorAll("p");
+console.log(allParagraphs);
+
+//1.4 Usa querySelector para mostrar por consola todos los elementos con 
+//	la clase.pokemon
+
+const AllPokemon = document.querySelectorAll (".pokemon");
+console.log (AllPokemon);
+
+
+
+//1.5 Usa querySelector para mostrar por consola todos los elementos con 
+//el atributo data-function="testMe".
+
+const showTestMe = document.querySelectorAll('[data-function="testMe]');
+console.log(showTestMe);
+
+//1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+//data-function="testMe".t
