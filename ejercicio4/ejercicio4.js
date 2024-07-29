e2223b0f24c9a17b4340724a9b2f3ac1917b6169
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-07-29 02:59:45 +0100

    4

--- ejercicio4/ejercicio4.js
+++ ejercicio4/ejercicio4.js
@@ -32,8 +32,30 @@ console.log(input.value);
 });
 
 
- 
+ //Util para informar al usuario, por ejemplo, de la debilidad o fortaleza de una contraseña a medida que la introduce
 
 
 
 //1.3 Añade un evento 'input' que ejecute un console.log con el valor del input. 
+
+input.addEventListener('input', function() {
+
+
+console.log(input.value);
+
+
+});
+
+
+//Util, por ejemplo, para el autocompletado o sugerencias mientras el usuario escribe
+
+//Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html 
+//que imprima cada uno de los albums.
+
+const albums = [
+  "De Mysteriis Dom Sathanas",
+  "Reign of Blood",
+  "Ride the Lightning",
+  "Painkiller",
+  "Iron Fist",
+];
